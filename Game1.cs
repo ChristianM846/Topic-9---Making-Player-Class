f4c79f90025aaf192bf17363ee0a93e608993ffb
Christian Moyes <moyechri846@gmail.com>
2023-12-13 10:55:41 -0500

    Good things

--- Game1.cs
+++ Game1.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Topic_9___Making_Player_Class
 {
     public class Game1 : Game
     {
+        Player amoeba;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -14,6 +17,9 @@ namespace Topic_9___Making_Player_Class
         Texture2D amoebaTexture;
         Texture2D wallTexture;
         Texture2D foodTexture;
+
+        List<Rectangle> barriers;
+        List<Rectangle> food;
 
 
         public Game1()
@@ -25,9 +31,19 @@ namespace Topic_9___Making_Player_Class
 
         protected override void Initialize()
         {
-            
+            barriers = new List<Rectangle>();
+            barriers.Add(new Rectangle(100, 100, 10, 200));
+            barriers.Add(new Rectangle(400, 400, 100, 10));
+
+            food = new List<Rectangle>();
+            food.Add(new Rectangle(50, 50, 10, 10));
+            food.Add(new Rectangle(600, 100, 10, 10));
+            food.Add(new Rectangle(50, 200, 10, 10));
 
             base.Initialize();
+            amoeba = new Player(amoebaTexture, 10, 10);
+
+
         }
 
         protected override void LoadContent()
@@ -45,6 +61,45 @@ namespace Topic_9___Making_Player_Class
                 Exit();
 
             keyboardState = Keyboard.GetState();
+            amoeba.HSpeed = 0;
+            amoeba.VSpeed = 0;
+
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                amoeba.VSpeed = -3;
+            }
+            else if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                amoeba.VSpeed = 3;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                amoeba.HSpeed = 3;
+            }
+            else if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                amoeba.HSpeed = -3;
+            }
+
+            amoeba.Update();
+
+            foreach (Rectangle barrier in barriers)
+            {
+                if (amoeba.Collide(barrier))
+                {
+                    amoeba.UndoMove();
+                }
+            }
+
+            for (int i = 0; i < food.Count; i++)
+            {
+                if (amoeba.Collide(food[i]))
+                {
+                    food.RemoveAt(i);
+                    i--;
+                }
+            }
 
 
             base.Update(gameTime);
@@ -53,8 +108,22 @@ namespace Topic_9___Making_Player_Class
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+
+            amoeba.Draw(_spriteBatch);
+
+            foreach (Rectangle barrier in barriers)
+            {
+                _spriteBatch.Draw(wallTexture, barrier, Color.White);
+            }
+
+            foreach (Rectangle bit in food)
+            {
+                _spriteBatch.Draw(foodTexture, bit, Color.Green);
+            }
 
 
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
--- Player.cs
+++ Player.cs
@@ -10,6 +10,25 @@ namespace Topic_9___Making_Player_Class
 {
     internal class Player
     {
+
+        enum PlayerLevel
+        {
+            level1,
+            level2,
+            level3,
+            level4,
+            level5,
+        }
+
+        private int _maxHealth;
+        private int _health;
+        private int _baseAttack;
+        private int _attack;
+        private int _baseDefence;
+        private int _defense;
+        private int _level;
+
+
         private Texture2D _texture;
         private Rectangle _location;
         private Vector2 _speed;
@@ -19,6 +38,7 @@ namespace Topic_9___Making_Player_Class
             _texture = texture;
             _location = new Rectangle(x, y, 30, 30);
             _speed = new Vector2();
+            _health = 100;
         }
 
         public float HSpeed
@@ -33,6 +53,38 @@ namespace Topic_9___Making_Player_Class
             set { _speed.Y = value; }
         }
 
+        public int Health
+        {
+            get { return _health; }
+            set { _health = value; }
+        }
+
+        private void Move()
+        {
+            _location.X += (int)_speed.X;
+            _location.Y += (int)_speed.Y;
+        }
+
+        public void Update()
+        {
+            Move();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _location, Color.White);
+        }
+
+        public bool Collide(Rectangle item)
+        {
+            return _location.Intersects(item);
+        }
+
+        public void UndoMove()
+        {
+            _location.X -= (int)_speed.X;
+            _location.Y -= (int)_speed.Y;
+        }
 
 
 
